669f098f7cd8c7b1ca5a733a6865fad2301d25a5
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-06 18:00:39 +0200

    icons installed

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,42 @@
 <template>
 	<v-app>
-		<v-main>
-			<v-container class="fill-height" fluid>
-				<v-row align="center" justify="center">
-					<v-col cols="12" sm="8" md="8">
-						<v-card class="elevation-12">
-							<v-window v-model="step"> </v-window>
-						</v-card>
-					</v-col>
-				</v-row>
-			</v-container>
-		</v-main>
+		<v-container class="fill-height" fluid>
+			<v-row align="center" justify="center">
+				<v-col cols="12" sm="8" md="8">
+					<v-card class="elevation-12">
+						<v-window v-model="step">
+							<v-window-item :value="1">
+								<v-row>
+									<v-col col="12" md="8">
+										<v-card-text class="mt-12">
+											<h1
+												class="text-center display-2 teal--text text--accent-3"
+											>
+												Sign in
+											</h1>
+											<div class="text-center mt-4">
+												<v-btn class="mx-2" color="black" fab outlined>
+													<v-icon>fab fa-facebook</v-icon>
+												</v-btn>
+												<v-btn class="mx-2" color="black" fab outlined>
+													<v-icon>fab fa-google-plus-g</v-icon>
+												</v-btn>
+												<v-btn class="mx-2" color="black" fab outlined>
+													<v-icon>fab fa-linkedin-in</v-icon>
+												</v-btn>
+											</div>
+										</v-card-text>
+									</v-col>
+									<v-col col="12" md="4"> </v-col>
+								</v-row>
+							</v-window-item>
+
+							<v-window-item :value="2"> </v-window-item>
+						</v-window>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
 	</v-app>
 </template>
 <script>
@@ -20,6 +46,9 @@
 			return {
 				step: 1,
 			};
+		},
+		props: {
+			source: String,
 		},
 	};
 </script>
