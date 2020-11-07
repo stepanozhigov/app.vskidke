4dcd9b93dec42b71a4e1b81c0a98bdf645f6b9a2
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-07 13:32:07 +0200

    routes protected

--- src/router/index.js
+++ src/router/index.js
@@ -14,40 +14,88 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {requiresAuth: true},
     children: [
       {
         path: '/dashboard',
         name: 'Dashboard',
+        meta: {requiresAuth: true},
         component: Dashboard
       },
       {
         path: '/projects',
         name: 'Projects',
+        meta: {requiresAuth: true},
         component: Projects
       },
       {
         path: '/team', 
         name: 'Team',
+        meta: {requiresAuth: true},
         component: Team
       },
-      {
-        path: '/login',
-        name: 'Login',
-        component: Login
-      },
-      {
-        path: '/register',
-        name: 'Register',
-        component: Register
-      }
     ]
   },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {guest: true},
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: {guest: true},
+    component: Register
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+//Authentication
+
+function loggedIn() {
+  //return localStorage.getItem('loggedIn')
+  return true;
+}
+
+router.beforeEach((to, from, next) => {
+
+  //undefined routes to /
+  if(to.matched.length < 1)  {
+    next({
+      path: '/',
+      //query: { redirect: to.fullPath }
+    })
+  }
+
+  //routes require auth
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    console.log(to.matched);
+
+    //needs authentication
+    if (!loggedIn()) {
+      next({
+        path: '/login',
+        //query: { redirect: to.fullPath }
+      })
+    }
+    //if authenticated already
+    else {
+      next()
+    }
+  }
+  
+  //routes not require auth
+  else if (to.matched.some(record => record.meta.guest)) {
+    next({
+      path: '/dashboard',
+    })
+  }
 })
 
 export default router
